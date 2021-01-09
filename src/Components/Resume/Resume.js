8ab05bf411b3a85340fcc85a9e28ea4b3f18bca7
Arthur Carson <rthrcrsn@gmail.com>
2021-01-09 14:41:26 -0600

    added new skills and new activities

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -8,10 +8,13 @@ import html from "../../Media/html.jpg";
 import jquery from "../../Media/jquery.jpg";
 import js from "../../Media/js.jpg";
 import material from "../../Media/material.jpg";
+import mongo from "../../Media/mongo.jpg";
+import mysql from "../../Media/mysql.jpg";
 import node from "../../Media/node.jpg";
 import postgres from "../../Media/postgres.jpg";
 import react from "../../Media/react.jpg";
 import redux from "../../Media/redux.jpg";
+import python from "../../Media/python.jpg";
 import resume from "../../Media/ArthurCarsonResume.pdf";
 
 const useStyles = makeStyles((theme) => ({
@@ -72,6 +75,18 @@ const useStyles = makeStyles((theme) => ({
       img: gitcat,
       title: 'GitHub'
     },
+    {
+      img: mongo,
+      title: 'MongoDB'
+    },
+    {
+      img: mysql,
+      title: 'MySQL'
+    },
+    {
+      img: python,
+      title: 'Python'
+    },
    ];
 
 function Resume() {
@@ -81,37 +96,49 @@ function Resume() {
     <Grid style={{marginBottom: '4em'}} id="resume" container direction="column" justify="center" alignItems="center" className={classes.root}>
       <Typography variant="h2">Resume</Typography>
       <Button target="_blank" rel="noreferrer" href={resume}  variant="outlined" size="small">
-          <Typography>View Resume PDF</Typography>
-        </Button>
+        <Typography>View Resume PDF</Typography>
+      </Button>
       <Typography variant="h4">SKILLS</Typography>
-      <Grid container direction="row" justify="space-evenly"><img className="icons" src={html} />
-      <img className="icons" src={css} alt="CSS logo"/>
-      <img className="icons" src={js} alt="Javascript logo"/>
-      <img className="icons" src={jquery} alt="jQuery logo"/>
+      <Grid container direction="row" justify="space-evenly">
+      <img className="icons" src={postgres} alt="PostgreSQL logo"/>
+      <img className="icons" src={mongo} alt="MongoDB logo"/>
+      <img className="icons" src={mysql} alt="MySQL logo"/>
       <img className="icons" src={react} alt="React logo"/>
       <img className="icons" src={redux} alt="Redux logo"/>
       <img className="icons" src={node} alt="Node logo"/>
+      <img className="icons" src={python} alt="Python logo"/>
+      <img className="icons" src={gitcat} alt="Github logo"/>
+      <img className="icons" src={jquery} alt="jQuery logo"/>
+      <img className="icons" src={js} alt="Javascript logo"/>
+      <img className="icons" src={html} alt="HTML logo"/>
+      <img className="icons" src={css} alt="CSS logo"/>
       <img className="icons" src={bootstrap} alt="Bootstrap logo"/>
       <img className="icons" src={material} alt="Material-UI logo"/>
-      <img className="icons" src={postgres} alt="PostgreSQL logo"/>
-      <img className="icons" src={gitcat} alt="Github logo"/></Grid>
+      </Grid>
       
-      <Typography variant="h6">
-        HTML, CSS, Javascript, jQuery, React, Redux.js, Node.js, Bootstrap, Material-UI, PostgreSQL, GitHub   
-      </Typography>
-      <Typography variant="h6">and Ajax/Axios</Typography>
+      <Typography variant="h6">PostgreSQL, MongoDB, MySQL,</Typography>
+      <Typography variant="h6">React.js, Redux.js, Node.js,</Typography>
+      <Typography variant="h6">Python, GitHub, jQuery,</Typography>
+      <Typography variant="h6">Javascript, HTML, CSS,</Typography>
+      <Typography variant="h6">Bootstrap, Material-UI</Typography>
+
       <Typography variant="h3">
         EXPERIENCE
       </Typography>
       <Typography variant="h4">Prime Digital Academy</Typography>
-          <Typography>Minneapolis, MN June 2020 - PRESENT</Typography>
+          <Typography>Minneapolis, MN June 2020 - Nov 2020</Typography>
           <Typography style={{"font-weight": "bold"}}>Full Stack Software Development Student</Typography>
+      <Typography variant="h4">Group Project: Playworks Step Challenge</Typography>
+          <Typography>Partnered with a team of four to build an app to make it easier for the PlayWorks organization to arrange and manage an event such as a step challenge or a virtual home run derby</Typography>
+          <Typography>Automated the formerly time consuming aspects of organizing and maintaining a competition within a large organization and between organizations such as curate posted photos, schedule daily challenges by date, update contest rules and FAQ, export contest results to CSV, and calculate leaderboards by contests and team</Typography>
+          <Typography style={{"font-weight": "bold"}}>Technologies Used: AWS, PostgreSQL, Redux.js, Axios, React.js, Node.js, Material-UI, Javascript, CSS, HTML</Typography>
       <Typography variant="h4">Solo Project: Cast Watch</Typography>
           <Typography>Built an app to make it easier to look up a movie or actor/actress to find out if anyone in the movie has been in anything else the user has seen</Typography>
           <Typography>Answers that pesky question of “What have I seen them in?” that people get when watching a movie</Typography>
           <Typography>Built accessing a third-party database to scan the cast of a movie and to scan the filmography of an actor/actress.</Typography> 
           <Typography>Compared that data to a user built personal library to find matches of films and actors/actresses</Typography>
-          <Typography style={{"font-weight": "bold"}}>Technologies Used: PostgresQL, APIs, Redux.js, Axios, React, Node.js, Material UI, Jquery, Javascript, CSS, HTML</Typography>
+          <Typography style={{"font-weight": "bold"}}>Technologies Used: PostgreSQL, Third-party APIs, Postman, Redux.js, Axios, React.js, Node.js, Material-UI, Javascript, CSS, HTML</Typography>
+      
       <Typography variant="h4">Moto-I</Typography>
           <Typography>Minneapolis, MN August 2019 - July 2020</Typography>
           <Typography style={{"font-weight": "bold"}}>Food Server</Typography>
